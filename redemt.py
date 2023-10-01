e0016f229d870a69b85255e6915a7e3c58aefa47
Cyberguru1 <hamzacypher34@gmail.com>
2023-10-01 13:04:14 -0500

    updated links

---
+++ redemt.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python 
+import re
+import requests
+import sys
+
+url = sys.argv[1]
+contents = requests.get(url).text
+
+pattern = r'<h1 id="([^"]+)"'
+
+match = re.findall(pattern, contents)
+if match:
+    id_content = match
+
+    for i in id_content:
+        print(f"- [{i}]({url}#{i}")
+    
+    for i in id_content:
+        print(f"{url}#{i}")
+
